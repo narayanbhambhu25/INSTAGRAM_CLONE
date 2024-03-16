25f571189738bd86a32247b6c46693bb9388d58d
Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
2024-03-16 19:33:08 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+const mongoose = require('mongoose')
+const {MONGOURI} = require('./keys')
+
+mongoose.connect(MONGOURI)
+mongoose.connection.on('connected',()=>{
+    console.log("conn to Mongo")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("error to conn to Mongo",err)
+})
+
+// 3sOi6x3n7xHMsbqu
+
+app.listen(PORT,()=>{
+    console.log("Running on",PORT)
+})
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Narayan:3sOi6x3n7xHMsbqu@cluster0.pag7xqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+}
