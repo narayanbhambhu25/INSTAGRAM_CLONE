bfe5fe7a54b4e499f2d74fb54fb5eb1e5bf960b6
Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
2024-03-16 19:44:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const mongoose = require('mongoose')
 const {MONGOURI} = require('./keys')
 
 mongoose.connect(MONGOURI)
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('connected', () => {
     console.log("conn to Mongo")
 })
-mongoose.connection.on('error',(err)=>{
-    console.log("error to conn to Mongo",err)
+mongoose.connection.on('error', (err) => {
+    console.log("error to conn to Mongo", err)
 })
 
 // 3sOi6x3n7xHMsbqu
 
-app.listen(PORT,()=>{
-    console.log("Runninggggg on",PORT)
+app.listen(PORT, () => {
+    console.log("Running on", PORT)
 })
