0bd5ed979cf4108e621f5a4960cabad62f6291fc
Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
2024-03-21 15:11:17 +0530

    Added Context
    Co-Authored-By: Rohitash001 <121787256+rohitash001@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,49 @@
-import React, { useEffect, createContext } from "react";
+import React, { useEffect, createContext, useReducer, useContext } from "react";
 import NavBar from "./components/Navbar";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./components/screens/Home";
 import Signin from "./components/screens/SignIn";
 import Signup from "./components/screens/Signup";
 import Profile from "./components/screens/Profile";
 import CreatePost from "./components/screens/Createpost";
+import { reducer, initialState } from "./reducers/userReducer";
 
-const UserContext = createContext();
+export const UserContext = createContext();
+
+const Routing = () => {
+  // will acccess history in this Routing
+  const navigate = useNavigate();
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user")); //JSON.parse() make this "user" object from string
+    if (user) {
+      dispatch({ type: "USER", payload: user });
+      navigate("/");
+    } else {
+      navigate("/signin");
+    }
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/create" element={<CreatePost />} />
+    </Routes>
+  );
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/create" element={<CreatePost />} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <NavBar />
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../App";
 import M from "materialize-css";
 
 const SignIn = () => {
+  const { state, dispatch } = useContext(UserContext);
   const navigate = useNavigate();
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -40,6 +42,7 @@ const SignIn = () => {
         } else {
           localStorage.setItem("jwt", data.token); // saven token to localstorage
           localStorage.setItem("user", JSON.stringify(data.user));
+          dispatch({ type: "USER", payload: data.user });
           M.toast({
             html: "SignedIn Success",
             classes: "#43a047 green daeken-1",
